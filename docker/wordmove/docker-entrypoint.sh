91d2157e65700947841034807ff9d776d3c90d2a
Matt Maiorano <maiorano84@hotmail.com>
2020-12-17 20:20:04 -0500

    Expand Wordmove to allow root SSH using volumes

--- docker/wordmove/docker-entrypoint.sh
+++ docker/wordmove/docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 eval `ssh-agent -s`
-find ~/.ssh -type f -not -name "*.pub" -not -name "known_hosts" -not -name "authorized_keys" -not -name "config" -exec ssh-add {} \;
+
+if [ ! -d "~/.ssh" ]; then
+  mkdir -p ~/.ssh
+fi
+
+cp -r /wordmove/.ssh/* ~/.ssh
+find ~/.ssh -type f -not -name "*.pub" -not -name "known_hosts" -not -name "authorized_keys" -not -name "config" -follow -print | while read -r f; do
+  chmod 600 $f
+  ssh-add $f
+done
 
 wordmove "$@"
